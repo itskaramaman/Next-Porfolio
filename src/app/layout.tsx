6f64febf1ea207e24573549551fc50e08a9fb00e
Karamjeet Singh <singhkaramjeetapple.com>
2025-01-11 21:53:52 -0800

    seo desc changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "Karamjeet Singh | Full Stack Developer Portfolio",
-  description:
-    "Explore Karamjeet Singh's portfolio showcasing expertise in Full Stack Development, Frontend and Backend Development, React, Next.js, JavaScript, and Software Development. Discover innovative projects, achievements, and industry-level skills.",
+  description: `Explore Karamjeet Singh's portfolio showcasing expertise in Full Stack Development, 
+  Frontend and Backend Development and Software Development. React, Next.js, JavaScript, TypeScript, Python, Express.js, Node.js, Flask, FastAPI . Discover innovative projects, achievements, and industry-level skills.`,
 };
 
 export default function RootLayout({
